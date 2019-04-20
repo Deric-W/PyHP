f88e24ea1d27ccffd74a3139582eae8c36753638
Eric W <robo-eric@gmx.de>
2019-04-20 17:54:34 +0200

    add SERVER, prepare for parser++, fix http_response_code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+import time
+REQUEST_TIME = time.time()
 import sys
+import os
 import re
 import cgi
 from collections import defaultdict
@@ -18,6 +21,53 @@ class pyhp:
 			"418":"I’m a teapot",
 			"500":"Internal Server Error"
 		}									#incomplete
+		try:								#to prevent IndexError
+			sys.argv[1]
+		except:
+			sys.argv.append("")
+		self.SERVER = {
+			"PyHP_SELF":os.getenv("SCRIPT_NAME",default=""),
+			"argv": os.getenv("QUERY_STRING",default=sys.argv[2:]),
+			"argc":len(sys.argv) - 2,
+			"GATEWAY_INTERFACE": os.getenv("GATEWAY_INTERFACE",default=""),
+			"SERVER_ADDR": os.getenv("SERVER_ADDR",default=""),
+			"SERVER_NAME": os.getenv("SERVER_NAME",default=""),
+			"SERVER_SOFTWARE": os.getenv("SERVER_SOFTWARE",default=""),
+			"SERVER_PROTOCOL": os.getenv("SERVER_PROTOCOL",default=""),
+			"REQUEST_METHOD": os.getenv("REQUEST_METHOD",default=""),
+			"REQUEST_TIME": int(REQUEST_TIME),
+			"REQUEST_TIME_FLOAT": REQUEST_TIME,
+			"QUERY_STRING": os.getenv("QUERY_STRING",default=""),
+			"DOCUMENT_ROOT": os.getenv("DOCUMENT_ROOT",default=""),
+			"HTTP_ACCEPT": os.getenv("HTTP_ACCEPT",default=""),
+			"HTTP_ACCEPT_CHARSET": os.getenv("HTTP_ACCEPT_CHARSET",default=""),
+			"HTTP_ACCEPT_ENCODING": os.getenv("HTTP_ACCEPT_ENCODING",default=""),
+			"HTTP_ACCEPT_LANGUAGE": os.getenv("HTTP_ACCEPT_LANGUAGE",default=""),
+			"HTTP_CONNECTION": os.getenv("HTTP_CONNECTION",default=""),
+			"HTTP_HOST": os.getenv("HTTP_HOST",default=""),
+			"HTTP_REFERER": os.getenv("HTTP_REFERER",default=""),
+			"HTTP_USER_AGENT": os.getenv("HTTP_USER_AGENT",default=""),
+			"HTTPS": os.getenv("HTTPS",default=""),
+			"REMOTE_ADDR": os.getenv("REMOTE_ADDR",default=""),
+			"REMOTE_HOST": os.getenv("REMOTE_HOST",default=""),
+			"REMOTE_PORT": os.getenv("REMOTE_PORT",default=""),
+			"REMOTE_USER": os.getenv("REMOTE_USER",default=""),
+			"REDIRECT_REMOTE_USER": os.getenv("REDIRECT_REMOTE_USER",default=""),
+			"SCRIPT_FILENAME": sys.argv[1],
+			"SERVER_ADMIN": os.getenv("SERVER_ADMIN",default=""),
+			"SERVER_PORT": os.getenv("SERVER_PORT",default=""),
+			"SERVER_SIGNATURE": os.getenv("SERVER_SIGNATURE",default=""),
+			"PATH_TRANSLATED": os.getenv("PATH_TRANSLATED",default=sys.argv[1]),
+			"SCRIPT_NAME": os.getenv("SCRIPT_NAME",default=os.path.basename(sys.argv[1])),
+			"REQUEST_URI": os.getenv("REQUEST_URI",default=""),
+			"PyHP_AUTH_DIGEST":"",
+			"PyHP_AUTH_USER":"",
+			"PyHP_AUTH_PW":"",
+			"AUTH_TYPE":"",
+			"PATH_INFO": os.getenv("PATH_INFO",default=""),
+			"ORIG_PATH_INFO": os.getenv("PATH_INFO",default="")
+		}										#incomplete too (AUTH)
+
 		self.response_code = [200,"OK"]
 		self.headers = []
 		self.header_sent = False
@@ -27,17 +77,17 @@ class pyhp:
 		for key in data:					#build $_REQUEST array from PHP
 			self.REQUEST[key] = data.getvalue(key)
 
-		try:
+		if sys.argv[1] != "":
 			file = open(sys.argv[1],"r",encoding='utf-8') #read file
 			self.file_content = file.read().split("\n")
 			file.close()
-		except IndexError: #file not given, read from stdin
+		else: #file not given, read from stdin
 			self.file_content = input().split("\n")
 
 		if self.file_content[0][:2] == "#!":				#shebang
-			self.file_content = self.strip_all_stn("\n".join(self.file_content[1:]))
+			self.file_content = self.mstrip("\n".join(self.file_content[1:]),["\n"," ","\t"])
 		else:
-			self.file_content = self.strip_all_stn("\n".join(self.file_content))
+			self.file_content = self.mstrip("\n".join(self.file_content),["\n"," ","\t"])
 
 		if self.file_content[:6] == "<?pyhp" and self.file_content[6] in ["\n"," ","\t"]: #if file starts with python code
 			self.code_at_begin = True
@@ -49,16 +99,42 @@ class pyhp:
 		if self.file_content[0] == "": #if match at begin
 			self.file_content = self.file_content[1:]	
 
-	def strip_all_stn(self,Text): #removes all \n,\t and " " from start and end of text
-		chars = ["\n"," ","\t"]
-		while Text[0] in chars:
-			Text = Text[1:]
-		while Text[-1] in chars:
-			Text = Text[:-1]
-		return Text
+	def mstrip(self,text,chars): #removes all chars in chars from start and end of text
+		while len(text) > 0 and text[0] in chars:
+			text = text[1:]
+		while len(text) > 0 and text[-1] in chars:
+			text = text[:-1]
+		return text
+
+	def get_indent(self,line):						#return string and index of indent
+		index = 0
+		string = ""
+		for char in line:
+			if char in [" ","\t"]:
+				index = index + 1
+				string = string + char
+			else:
+				break
+		return [index,string]
+
+	def is_comment(self,line):						# return True if line is comment
+		comment = False
+		for char in line:
+			if char in [" ","\t"]:
+				pass
+			elif char == "#":
+				comment = True
+				break
+			else:
+				comment = False
+				break
+		return comment
+
+	def fix_indent(self,code):
+		pass 										#todo
 	
 	def http_response_code(self,response_code=None): #set response code
-		old_response_code = self.response_code
+		old_response_code = self.response_code[0]
 		if response_code != None:
 			self.response_code = [int(response_code),self.response_messages[str(response_code)]]
 		return old_response_code
