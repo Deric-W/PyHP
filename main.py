de8e982fee067a0994acbaab19a399b6c4a849f8
Eric W <robo-eric@gmx.de>
2019-05-01 15:25:32 +0200

    tiny cleanup, preparation for improved parser

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ import cgi
 from collections import defaultdict
 
 class pyhp:
-	def __init__(self):
-		self.print = print		#backup for sending headers
-		self.response_messages = {
+	def __init__(self):		
+		if len(sys.argv) > 1:																	#file or parameter exist
+			if sys.argv[1] == "-c":																#chache enabled
+				self.caching = True
+				try:
+					self.file_path = sys.argv[2]
+				except IndexError:
+					self.file_path = ""
+			else:
+				self.file_path = sys.argv[1]
+		else:
+			self.file_path = ""
+
+
+		self.response_messages = {																#incomplete
 			"200":"OK",
 			"300":"Multiple Choices",
 			"301":"Moved Permanently",
@@ -20,12 +32,9 @@ class pyhp:
 			"404":"Not Found",
 			"418":"I’m a teapot",
 			"500":"Internal Server Error"
-		}									#incomplete
-		try:								#to prevent IndexError
-			sys.argv[1]
-		except:
-			sys.argv.append("")
-		self.SERVER = {
+		}
+
+		self.SERVER = {																			#incomplete too (AUTH)
 			"PyHP_SELF":os.getenv("SCRIPT_NAME",default=""),
 			"argv": os.getenv("QUERY_STRING",default=sys.argv[2:]),
 			"argc":len(sys.argv) - 2,
@@ -53,12 +62,12 @@ class pyhp:
 			"REMOTE_PORT": os.getenv("REMOTE_PORT",default=""),
 			"REMOTE_USER": os.getenv("REMOTE_USER",default=""),
 			"REDIRECT_REMOTE_USER": os.getenv("REDIRECT_REMOTE_USER",default=""),
-			"SCRIPT_FILENAME": sys.argv[1],
+			"SCRIPT_FILENAME": self.file_path,
 			"SERVER_ADMIN": os.getenv("SERVER_ADMIN",default=""),
 			"SERVER_PORT": os.getenv("SERVER_PORT",default=""),
 			"SERVER_SIGNATURE": os.getenv("SERVER_SIGNATURE",default=""),
-			"PATH_TRANSLATED": os.getenv("PATH_TRANSLATED",default=sys.argv[1]),
-			"SCRIPT_NAME": os.getenv("SCRIPT_NAME",default=os.path.basename(sys.argv[1])),
+			"PATH_TRANSLATED": os.getenv("PATH_TRANSLATED",default=self.file_path),
+			"SCRIPT_NAME": os.getenv("SCRIPT_NAME",default=os.path.basename(self.file_path)),
 			"REQUEST_URI": os.getenv("REQUEST_URI",default=""),
 			"PyHP_AUTH_DIGEST":"",
 			"PyHP_AUTH_USER":"",
@@ -66,47 +75,48 @@ class pyhp:
 			"AUTH_TYPE":"",
 			"PATH_INFO": os.getenv("PATH_INFO",default=""),
 			"ORIG_PATH_INFO": os.getenv("PATH_INFO",default="")
-		}										#incomplete too (AUTH)
+		}
 
+		self.print = print																			#backup for sending headers
 		self.response_code = [200,"OK"]
 		self.headers = []
 		self.header_sent = False
 
-		data = cgi.FieldStorage()
+		data = cgi.FieldStorage()																	#build $_REQUEST array from PHP
 		self.REQUEST = defaultdict(lambda: "")
-		for key in data:					#build $_REQUEST array from PHP
-			self.REQUEST[key] = data.getvalue(key)
+		for key in data:
+			self.REQUEST[key] = data.getvalue(key)													#to contain lists instead of multiple FieldStorages if key has multiple values
 
-		if sys.argv[1] != "":
-			file = open(sys.argv[1],"r",encoding='utf-8') #read file
+		if self.file_path != "":
+			file = open(self.file_path,"r",encoding='utf-8') 										#read file
 			self.file_content = file.read().split("\n")
 			file.close()
-		else: #file not given, read from stdin
+		else: 																						#file not given, read from stdin
 			self.file_content = input().split("\n")
 
-		if self.file_content[0][:2] == "#!":				#shebang
-			self.file_content = self.mstrip("\n".join(self.file_content[1:]),["\n"," ","\t"])
+		if self.file_content[0][:2] == "#!":														#shebang support
+			self.file_content = self.mstrip("\n".join(self.file_content[1:]),["\n"," ","\t"])		#strip invisible chars from start and end
 		else:
-			self.file_content = self.mstrip("\n".join(self.file_content),["\n"," ","\t"])
+			self.file_content = self.mstrip("\n".join(self.file_content),["\n"," ","\t"])			#strip invisible chars from start and end
 
-		if self.file_content[:6] == "<?pyhp" and self.file_content[6] in ["\n"," ","\t"]: #if file starts with python code
+		if self.file_content[:6] == "<?pyhp" and self.file_content[6] in ["\n"," ","\t"]: 			#if file starts with python code
 			self.code_at_begin = True
 		else:
 			self.code_at_begin = False
 		
 		self.file_content = re.split("\<\?pyhp[\n \t]",self.file_content)
 		self.first_section = True
-		if self.file_content[0] == "": #if match at begin
+		if self.file_content[0] == "": 																#if match at begin
 			self.file_content = self.file_content[1:]	
 
-	def mstrip(self,text,chars): #removes all chars in chars from start and end of text
+	def mstrip(self,text,chars): 																	#removes all chars in chars from start and end of text
 		while len(text) > 0 and text[0] in chars:
 			text = text[1:]
 		while len(text) > 0 and text[-1] in chars:
 			text = text[:-1]
 		return text
 
-	def get_indent(self,line):						#return string and index of indent
+	def get_indent(self,line):																		#return string and index of indent
 		index = 0
 		string = ""
 		for char in line:
@@ -117,7 +127,7 @@ class pyhp:
 				break
 		return [index,string]
 
-	def is_comment(self,line):						# return True if line is comment
+	def is_comment(self,line):																		#return True if line is comment (first char == #)
 		comment = False
 		for char in line:
 			if char in [" ","\t"]:
@@ -130,64 +140,78 @@ class pyhp:
 				break
 		return comment
 
-	def fix_indent(self,code):
-		pass 										#todo
+	def fix_indent(self,code,section):
+		fixed_code = ""
+		linecount = 0
+		first_line = True
+		for line in code.split("\n"):
+			linecount = linecount + 1
+			if line.replace(" ","").replace("\n","") != "":											#not empthy
+				if not self.is_comment(line):
+					if first_line:
+						indent = self.get_indent(line)
+						first_line = False
+					if len(line) > indent[0] and line[:indent[0]] == indent[1]:						#line is big enough for indent and indent is the same as first line
+						fixed_code = fixed_code + line[indent[0]:] + "\n"
+					else:
+						raise IndentationError("File: " + self.file_path + " line: " + str(linecount) + " section: " + str(section))
+		return fixed_code
 	
-	def http_response_code(self,response_code=None): #set response code
+	def http_response_code(self,response_code=None): 												#set response code
 		old_response_code = self.response_code[0]
 		if response_code != None:
 			self.response_code = [int(response_code),self.response_messages[str(response_code)]]
 		return old_response_code
 	
-	def headers_list(self):		#list current header
+	def headers_list(self):																			#list current header
 		headers = []
 		for header in self.headers:
 			headers.append(str(header[0]) + ": " + str(header[1]))
 		return headers
 	
-	def header(self,header,replace=True,response_code=None):	#add headers and set response code
+	def header(self,header,replace=True,response_code=None):										#add headers and set response code
 		if response_code != None:
-			self.http_response_code(response_code)				#update response code if given
-		header = header.split("\n")[0]							#to prevent Header-Injection
+			self.http_response_code(response_code)													#update response code if given
+		header = header.split("\n")[0]																#to prevent Header-Injection
 		header = header.split(":")
 		header = [header[0].strip(" "),header[1].strip(" ")]
 		if replace:
 			new_header = []
 			for stored_header in self.headers:
 				if stored_header[0].lower() != header[0].lower():
-					new_header.append(stored_header) #same headers not in list
+					new_header.append(stored_header) 												#same header not in list
 			new_header.append(header)
 			self.headers = new_header
 		else:
 			self.headers.append(header)
 	
-	def header_remove(self,header):	#remove header
+	def header_remove(self,header):																	#remove header
 		header = header.split(":")
 		header = [header[0].strip(" "),header[1].strip(" ")]
 		new_header = []
 		for stored_header in self.headers:
 			if stored_header[0].lower() != header[0].lower() or stored_header[1].lower() != header[1].lower():
-				new_header.append(stored_header) #same headers not in list
+				new_header.append(stored_header) 													#same headers not in list
 		self.headers = new_header
 	
-	def headers_sent(self):		#true if headers already sent
+	def headers_sent(self):																			#true if headers already sent
 		return self.header_sent
 	
 	def sent_header(self):
-		self.print("Status: " + str(self.response_code[0]) + " " + self.response_code[1]) #print status code
-		mistake = True		#no content-type header
+		self.print("Status: " + str(self.response_code[0]) + " " + self.response_code[1]) 			#print status code
+		mistake = True																				#no content-type header
 		for header in self.headers:
-			if header[0].lower() == "content-type":	#check for content-type
+			if header[0].lower() == "content-type":													#check for content-type
 				mistake = False
-			self.print(str(header[0]) + ": " + str(header[1])) #sent header
+			self.print(str(header[0]) + ": " + str(header[1])) 										#sent header
 		if mistake:
-			self.print("Content-Type: text/html")
-		self.print()		#end of headers
+			self.print("Content-Type: text/html")													#sent fallback Content-Type header
+		self.print()																				#end of headers
 		self.header_sent = True
 	
 pyhp = pyhp()
 
-def print(*args,**kwargs):					#wrap print to auto sent headers
+def print(*args,**kwargs):																			#wrap print to auto sent headers
 	if not pyhp.header_sent:
 		pyhp.sent_header()
 	pyhp.print(*args,**kwargs)
