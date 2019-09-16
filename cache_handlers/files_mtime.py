e3939a103f83584307ab726f3dbd1b9255e43446
Eric W <robo-eric@gmx.de>
2019-09-16 22:22:03 +0200

    optimize is_available, use __method/attribute

--- cache_handlers/files_mtime.py
+++ cache_handlers/files_mtime.py
@@ -9,15 +9,15 @@ from time import time
 
 class handler:
     def __init__(self, cache_path, file_path, config):
-        self.cache_prefix = cache_path
-        self.cache_path = os.path.join(os.path.expanduser(cache_path), file_path.strip(os.path.sep) + ".cache")  # use full path to allow indentical named files in different directories with cache_path as root
-        self.file_path = file_path
-        self.ttl = config.getint("ttl")
-        self.max_size = config.getint("max_size")
+        self.__cache_prefix = cache_path
+        self.__cache_path = os.path.join(os.path.expanduser(cache_path), file_path.strip(os.path.sep) + ".cache")  # use full path to allow indentical named files in different directories with cache_path as root
+        self.__file_path = file_path
+        self.__ttl = config.getint("ttl")
+        self.__max_size = config.getint("max_size")
 
-    def get_cachedir_size(self):																	# get size of cache directory (with all sub directories) in Mbytes
+    def __get_cachedir_size(self):																	# get size of cache directory (with all sub directories) in Mbytes
         size = 0
-        for dirpath, dirnames, filenames in os.walk(self.cache_prefix):
+        for dirpath, dirnames, filenames in os.walk(self.__cache_prefix):
             size += os.path.getsize(dirpath)														# dont forget the size of the folder
             for filename in filenames:
                 filepath = os.path.join(dirpath, filename)
@@ -26,17 +26,17 @@ class handler:
         return size/(1024**2)                                                                       # bytes --> Mbytes
 
     def is_available(self):                                                                         # if cache directory has free space or the cached file is already existing or max_size < 0
-        if self.get_cachedir_size() < self.max_size or os.path.isfile(self.cache_path) or self.max_size < 0:
+        if self.__max_size < 0 or os.path.isfile(self.__cache_path) or self.__get_cachedir_size() < self.__max_size:
             return True
         else:
             return False
 
     def is_outdated(self):                                                                          # return True if cache is not created or needs refresh or exceeds ttl
-        if os.path.isfile(self.cache_path):                                                         # to prevent Exception if cache not existing
-            cache_mtime = os.path.getmtime(self.cache_path)
-            file_mtime = os.path.getmtime(self.file_path)
+        if os.path.isfile(self.__cache_path):                                                       # to prevent Exception if cache not existing
+            cache_mtime = os.path.getmtime(self.__cache_path)
+            file_mtime = os.path.getmtime(self.__file_path)
             age = time() - cache_mtime
-            if cache_mtime < file_mtime or age > self.ttl > -1:                                      # age > ttl > -1 ignores ttl if -1 or lower
+            if cache_mtime < file_mtime or age > self.__ttl > -1:                                    # age > ttl > -1 ignores ttl if -1 or lower
                 return True
             else:
                 return False
@@ -44,17 +44,17 @@ class handler:
             return True
 
     def load(self):
-        with open(self.cache_path, "rb") as cache:
+        with open(self.__cache_path, "rb") as cache:
             cache_content = marshal.load(cache)
         if len(cache_content) != 2:
-            raise ValueError("corrupted cache at " + self.cache_path)
+            raise ValueError("corrupted cache at " + self.__cache_path)
         else:
             return cache_content[0], cache_content[1]                                               # file_content, code_at_begin
 
     def save(self, file_content, code_at_begin):
-        if not os.path.isdir(os.path.dirname(self.cache_path)):                                 # directories not already created
-            makedirs(os.path.dirname(self.cache_path), exist_ok=True)
-        with open(self.cache_path, "wb") as cache:
+        if not os.path.isdir(os.path.dirname(self.__cache_path)):                                   # directories not already created
+            makedirs(os.path.dirname(self.__cache_path), exist_ok=True)
+        with open(self.__cache_path, "wb") as cache:
             marshal.dump([file_content, code_at_begin], cache)
 
     def close(self):
