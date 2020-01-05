a6b28781ff00b3137913feaed023a24d74c53207
Eric Wolf <robo-eric@gmx.de>
2020-01-05 20:10:34 +0100

    further cleanup and moving the code for isolation of python code to embed

---
+++ src/embed.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+# Module for processing strings embedded in text files, preferably Python code.
+# This module is part of the PyHP interpreter (https://github.com/Deric-W/PyHP-Interpreter)
+
+import re
+import marshal
+import sys
+from io import StringIO
+from contextlib import redirect_stdout
+
+__all__ = []
+
+# class for handling strings
+class FromString:
+    # get string, regex to isolate code and optional flags for the regex (default for processing text files)
+    # the userdata is given to the processor function to allow state
+    def __init__(self, string, regex, flags=re.MULTILINE|re.DOTALL, userdata=None):
+        self.sections = re.split(regex, string, flags=flags)
+        self.userdata = userdata
+
+    # process string with the code replaced by the output of the processor function
+    # this will modify self.sections
+    def process(self, processor):
+        code_sections = 0
+        # the first section is always not code, and every code section has string sections as neighbors
+        for i in range(1, len(self.sections), 2):
+            code_sections += 1
+            self.sections[i] = processor(self.sections[i], self.userdata)
+        return code_sections
+
+    # process the string and write the string and replaced code parts to file (default is sys.stdout)
+    # this will not modify self.sections
+    def execute(self, processor, file=sys.stdout):
+        code_sections = 0
+        for i in range(0, len(self.sections)):
+            code_sections += 1
+            if i % 2 == 1:  # uneven index --> code
+                file.write(processor(self.sections[i], self.userdata))
+            else:           # even index --> not code
+                file.write(self.sections[i])
+        return code_sections
+
+    def __str__(self):
+        return "".join(self.sections)
+
+# wrapper class for handling presplit strings
+class FromIter(FromString):
+    # get presplit string as iterator
+    def __init__(self, iterator, userdata=None):
+        self.sections = list(iterator) 
+        self.userdata = userdata        
+
+# function for processing python code
+def python_process(code, local_var={}):
+    stdout = StringIO()
+    with redirect_stdout(stdout):
+        # execute in seperatet namespace
+        exec(code, globals(), local_var)
+    output = stdout.getvalue()
+    stdout.close()
+    return output
+
+# function for aligning python code in case of a startindentation
+def python_align(code, indentation=None):
+    line_num = 0
+    code = code.split("\n")     # split to lines
+    for line in code:
+        line_num += 1
+        if not (not line or line.isspace() or python_is_comment(line)): # ignore non code lines
+            if indentation == None:     # first line of code, get startindentation
+                indentation = python_get_indentation(line)
+            if line.startswith(indentation): # if line starts with startindentation
+                code[line_num - 1] = line[len(indentation):]  # remove startindentation
+            else:
+                raise IndentationError("File: code processed by python_align Line: %s" % line_num)  # raise Exception on bad indentation
+    return "\n".join(code)  # join the lines back together
+                    
+
+# function for getting the indentation of a line of python code
+def python_get_indentation(line):
+    indentation = ""
+    for char in line:
+        if char in " \t":
+            indentation += char
+        else:
+            break
+    return indentation
+
+# check if complete line is a comment
+def python_is_comment(line):
+    return line.strip(" \t").startswith("#")
