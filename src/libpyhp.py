bed67ecb97fa264b2e6700e166108e71e80ce90d
Eric Wolf <robo-eric@gmx.de>
2020-01-09 20:59:26 +0100

    add header functios to libpyhp and fix __FILE__

--- src/libpyhp.py
+++ src/libpyhp.py
@@ -18,13 +18,18 @@ __all__ = ["PyHP", "dummy_cache_handler", "dummy_session_handler", "parse_get", 
 # class containing the implementations
 class PyHP:
     def __init__(self,    # build GET, POST, COOKIE, SERVER, REQUEST
+                file_path = sys.argv[0],    # override if not directly executed
                 request_order = ("GET", "POST", "COOKIE"),      # order in wich REQUEST gets updated
                 keep_blank_values = True,   # if to not remove "" values
                 fallback_value = None,      # fallback value of GET, POST, REQUEST and COOKIE if not None
                 enable_post_data_reading = False,   # if not to parse POST and consume stdin in the process
                 default_mimetype = "text/html"      # Content-Type header if not been set
                 ):
-        self.__FILE__ = os.path.abspath(sys.argv[0])    # absolute path of script
+        self.__FILE__ = os.path.abspath(file_path)    # absolute path of script
+        self.response_code = 200
+        self.headers = [["Content-Type", default_mimetype]]
+        self.header_sent = False
+        self.header_callback = lambda: None
 
         self.SERVER = {                                                                           # incomplete (AUTH)
             "PyHP_SELF": os.path.relpath(self.__FILE__, os.getenv("DOCUMENT_ROOT", default=os.curdir)),
@@ -89,8 +94,78 @@ class PyHP:
             else:   # ignore unknown methods
                 pass
 
+    # set new response code return the old one
+    # if no code has been set it will return 200
+    def http_response_code(self, response_code=None):
+        old_code = self.response_code
+        if response_code is not None:
+            self.response_code = response_code
+        return old_code     # is the current one if no response code has been provided
 
+    # set http header
+    # if replace=True replace existing headers of the same type, else simply add
+    def header(self, header, replace=True, http_response_code=None):
+        header = header.split("\n")[0]  # prevent header injection
+        header = header.split(":", maxsplit=1)  # split header into name and value
+        header = [part.strip() for part in header]     # remove whitespace
+        if len(header) == 1:     # no value provided
+            header.append("")   # add empthy value
+        if replace:
+            repleaced = False   # indicate if headers of the same type were found
+            for i in range(0, len(self.headers)):   # need index for changing headers
+                if self.headers[i][0].lower() == header[0].lower():     # found same header
+                    self.headers[i][1] = header[1]     # repleace
+                    repleaced = True
+            if not repleaced:   # header not already set
+                self.headers.append(header)
+        else:
+            self.headers.append(header)
 
+    # list set headers            
+    def headers_list(self):
+        headers = []
+        for header in self.headers:
+            headers.append(": ".join(header))   # add header like received by the client
+        return headers
+
+    # remove header with matching name
+    def header_remove(self, name):
+        name = name.lower()
+        self.headers = [header for header in self.headers if header[0].lower() != name] # remove headers with same name
+
+    # return if header have been sent
+    # unlike the PHP function it does not have file and line arguments
+    def headers_sent(self):
+        return self.header_sent
+
+    # set calback to be executed just before headers are send
+    # callback gets no arguments and the return value is ignored
+    def header_register_callback(self, callback):
+        if not self.header_sent:
+            self.header_callback = callback
+            return True
+        else:
+            return False
+
+    # send headers and execute callback
+    def send_headers(self):
+        if not self.header_sent:    # send
+            self.header_sent = True     # prevent recursion if callback prints output or headers set
+            self.header_callback()      # execute callback
+            print("Status:" , self.response_code, http.HTTPStatus(self.response_code).phrase)
+            for header in self.headers:
+                print(": ".join(header))
+            print()                     # end of headers
+        else:   # already sent
+            pass
+
+    # make wrapper for target function to call send_headers if wrapped function is used, like print
+    def make_header_wrapper(self, target=sys.stdout):
+        def wrapper(*args, **kwargs):
+            if not self.header_sent:
+                self.send_headers()
+            target(*args, **kwargs)     # call target with arguments
+        return wrapper
 
 def parse_get(keep_blank_values=True):
     return urllib.parse.parse_qs(os.getenv("QUERY_STRING", default=""), keep_blank_values=keep_blank_values)
@@ -153,7 +228,7 @@ class dummy_cache_handler:
         pass
 
     def load(self):
-        return ("WARNING: This is the dummy cache handler of the libpyhp module, iam providing no useful functions and are just fallback", )     # return warning
+        return ("WARNING: This is the dummy cache handler of the libpyhp module, iam providing no useful functions and are just a fallback", )     # return warning
 
     def close(self):
         pass
