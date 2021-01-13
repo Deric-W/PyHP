5e1a31f44c02432653d0e2334840d8d7f3fc4ae0
Eric Wolf <robo-eric@gmx.de>
2021-01-13 21:06:14 +0100

    replace travis tests with github actions unit tests
    This commit migrates the tests structure to github actions and replaces them with unittests,
    as suggested in issue #21.
    The tests are currently dummies to test the new structure.

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Unit tests for the PyHP cli"""
+
+import unittest
+
+# TODO: convert tests from .travis.yml to unittest test cases
+
+
+class Dummy(unittest.TestCase):
+    """Test dummy to allow coverage to work"""
+    def test_dummy(self) -> None:
+        """test that never fails"""
+        self.assertTrue(True)
