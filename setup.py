9d0488bebc90bbaf428d42dbe936a526e66f89fe
Eric Wolf <robo-eric@gmx.de>
2021-01-13 15:49:48 +0100

    replace setup.py with setup.cfg To provide a central place for configuration i replace setup.py. This change will allow for further configuration related to issue #21 to be placed in setup.cfg.

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
 #!/usr/bin/python3
 
+"""file to allow non PEP 517 builds"""
+
 import setuptools
-import pyhp
 
-with open("README.md", "r") as fd:
-    long_description = fd.read()
-
-setuptools.setup(
-    name="pyhp-core",   # pyhp was already taken
-    license="LICENSE",
-    version=pyhp.__version__,
-    author=pyhp.__author__,
-    author_email=pyhp.__email__,
-    description="package for embedding and using python code like php",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url=pyhp.__contact__,
-    packages=["pyhp"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ],
-    python_requires='>=3.5',
-)
+setuptools.setup()
