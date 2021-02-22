64169faabdbf28e2bd9a9d9537dbbccb8dd89e59
Eric Wolf <robo-eric@gmx.de>
2021-02-22 21:31:05 +0100

    add zipfile implementations of CodeSource and CodeSourceContainer

--- pyhp/caching/zipfiles.py
+++ pyhp/caching/zipfiles.py
@@ -11,3 +11,191 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-only
+
+from __future__ import annotations
+import os
+import io
+import zipfile
+from datetime import datetime
+from importlib.abc import InspectLoader
+from importlib.machinery import ModuleSpec
+from locale import getpreferredencoding
+from types import CodeType
+from typing import IO, Union, Mapping, Any, Iterator, Optional
+from . import (
+    TimestampedCodeSource,
+    DirectCodeSource,
+    CodeSourceContainer
+)
+from ..compiler import Code
+from ..compiler.util import Compiler
+
+
+__all__ = (
+    "ENCODING",
+    "ZIPSource",
+    "ZIPFile"
+)
+
+ENCODING = getpreferredencoding(False)
+
+
+class ZIPLoader(InspectLoader):
+    """Loader to allow for source Introspection"""
+    __slots__ = ("name", "file", "entry")
+
+    name: str
+
+    file: zipfile.ZipFile
+
+    entry: zipfile.ZipInfo
+
+    def __init__(self, name: str, file: zipfile.ZipFile, entry: zipfile.ZipInfo) -> None:
+        self.name = name
+        self.file = file
+        self.entry = entry
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, ZIPLoader):
+            return self.name == other.name \
+                and self.file == other.file \
+                and self.entry == other.entry
+        return NotImplemented
+
+    def get_code(self, fullname: str) -> Optional[CodeType]:
+        """return None because pyhp files have no regular code object"""
+        if fullname != self.name:
+            raise ImportError(f"loader for '{self.name}' cannot handle '{fullname}'")
+        return None
+
+    def get_source(self, fullname: str) -> str:
+        if fullname != self.name:
+            raise ImportError(f"loader for '{self.name}' cannot handle '{fullname}'")
+        with io.TextIOWrapper(self.file.open(self.entry, "r"), ENCODING) as fd:
+            return fd.read()
+
+
+class ZIPSource(TimestampedCodeSource, DirectCodeSource):
+    """code source for accessing zip file entries"""
+    __slots__ = ("reader", "entry", "compiler", "spec")
+
+    reader: IO[bytes]
+
+    entry: zipfile.ZipInfo
+
+    compiler: Compiler
+
+    spec: ModuleSpec
+
+    def __init__(self, zipfile: zipfile.ZipFile, entry: zipfile.ZipInfo, compiler: Compiler) -> None:
+        self.reader = zipfile.open(entry, "r")
+        self.entry = entry
+        self.compiler = compiler
+        if isinstance(zipfile.filename, str):
+            origin = os.path.join(zipfile.filename, entry.filename)
+            has_location = True
+        else:
+            origin = f"<zip entry '{entry.filename}'>"
+            has_location = False
+        self.spec = ModuleSpec(
+            "__main__",
+            ZIPLoader("__main__", zipfile, entry),
+            origin=origin,
+            is_package=False
+        )
+        self.spec.has_location = has_location
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, ZIPSource):
+            return self.entry == other.entry \
+                and self.compiler == other.compiler
+        return NotImplemented
+
+    def code(self) -> Code:
+        """load and compile the code object from the zipfile"""
+        return self.compiler.compile_raw(self.source(), self.spec)
+
+    def source(self) -> str:
+        """retrieve the source code"""
+        self.reader.seek(0)     # in case this isnt the first read
+        return self.reader.read().decode(ENCODING)
+
+    def size(self) -> int:
+        """retrieve the size of the source code in bytes"""
+        return self.entry.file_size
+
+    def mtime(self) -> int:
+        """retrieve the modification timestamp in ns"""
+        return int(datetime(*self.entry.date_time).timestamp() * 1e+9)
+
+    def ctime(self) -> int:
+        """retireve the creation timestmp in ns"""
+        return 0    # not available
+
+    def atime(self) -> int:
+        """retrieve the access timestamp in ns"""
+        return 0    # not available
+
+    def close(self) -> None:
+        """close the reader"""
+        self.reader.close()
+
+
+class ZIPFile(CodeSourceContainer[ZIPSource]):
+    """CodeSourceContainer wrapping zipfile.ZipFile"""
+    __slots__ = ("file", "compiler")
+
+    file: zipfile.ZipFile
+
+    compiler: Compiler
+
+    @classmethod
+    def from_config(cls, config: Mapping[str, Any], before: Union[Compiler, CodeSourceContainer]) -> ZIPFile:
+        """create an instance from configuration data containing path and mode"""
+        if isinstance(before, Compiler):
+            path = config["path"]
+            if isinstance(path, str):
+                mode = config.get("mode", "r")  # default
+                if isinstance(mode, str):
+                    file = zipfile.ZipFile(path, mode)
+                    try:
+                        pwd = config["pwd"].encode("utf8")
+                    except KeyError:
+                        pass
+                    except Exception as e:
+                        file.close()
+                        raise ValueError("error while handling value of key 'pwd'") from e
+                    else:
+                        file.setpassword(pwd)
+                    return cls(file, before)
+                raise ValueError("expected value of key 'mode' to be a str")
+            raise ValueError("expected value of key 'path' to be a str")
+        raise ValueError(f"{cls.__name__} can not be used to decorate another CodeSourceContainer")
+
+    def __init__(self, file: zipfile.ZipFile, compiler: Compiler) -> None:
+        """create an instance from a zipfile and a compiler object"""
+        self.file = file
+        self.compiler = compiler
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, ZIPFile):
+            return self.file.filename == other.file.filename \
+                and self.compiler == other.compiler
+        return NotImplemented
+
+    def __getitem__(self, name: str) -> ZIPSource:
+        return ZIPSource(
+            self.file,
+            self.file.getinfo(name),
+            self.compiler
+        )
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self.file.namelist())
+
+    def __len__(self) -> int:
+        return len(self.file.infolist())
+
+    def close(self) -> None:
+        """close the wrapped ZipFile"""
+        self.file.close()
---
+++ tests/caching/test_zipfiles.py
@@ -0,0 +1,245 @@
+#!/usr/bin/python3
+
+"""Tests for pyhp.caching.zipfiles"""
+
+import unittest
+import os
+import re
+import tempfile
+import zipfile
+import inspect
+import importlib.util
+from importlib.machinery import ModuleSpec
+from pyhp.compiler import parsers, util, generic
+from pyhp.caching import zipfiles
+
+compiler = util.Compiler(
+    parsers.RegexParser(
+        re.compile(r"<\?pyhp\s"),
+        re.compile(r"\s\?>")
+    ),
+    util.Dedenter(
+        generic.GenericCodeBuilder(-1)
+    )
+)
+
+compiler2 = util.Compiler(
+    parsers.RegexParser(
+        re.compile(r"<\?pyhp\s"),
+        re.compile(r"\s\?>")
+    ),
+    generic.GenericCodeBuilder(0)   # different than compiler!
+)
+
+
+class TestZIPSource(unittest.TestCase):
+    """test ZIPSource"""
+    def test_eq(self) -> None:
+        """test ZIPSource.__eq__"""
+        with zipfile.ZipFile("tests/embedding.zip", "r") as file:
+            sources = [
+                zipfiles.ZIPSource(
+                    file,
+                    file.getinfo("syntax.pyhp"),
+                    compiler
+                ),
+                zipfiles.ZIPSource(
+                    file,
+                    file.getinfo("syntax.output"),
+                    compiler,
+                ),
+                zipfiles.ZIPSource(
+                    file,
+                    file.getinfo("syntax.pyhp"),
+                    compiler2
+                )
+            ]
+            try:
+                for source in sources:
+                    self.assertEqual(
+                        [source],
+                        [s for s in sources if s == source]
+                    )
+            finally:
+                for source in sources:
+                    source.close()
+
+    def test_code(self) -> None:
+        """test ZIPSource.code"""
+        with open("tests/embedding/syntax.pyhp", "r") as fd, \
+                zipfile.ZipFile("tests/embedding.zip") as file, \
+                zipfiles.ZIPSource(file, file.getinfo("syntax.pyhp"), compiler) as source:
+            code1 = source.code()
+            spec = ModuleSpec(
+                "__main__",
+                zipfiles.ZIPLoader("__main__", file, file.getinfo("syntax.pyhp")),
+                origin="tests/embedding.zip/syntax.pyhp",
+                is_package=False
+            )
+            spec.has_location = True
+            code2 = compiler.compile_raw(
+                fd.read(),
+                spec
+            )
+            code3 = source.code()   # check if the source can be read multiple times
+            self.assertEqual(code1, code2)
+            self.assertEqual(code1, code3)
+
+    def test_spec(self) -> None:
+        """test code spec"""
+        # the raw fd gets not closed somehow, maybe a bug?
+        with zipfile.ZipFile("tests/embedding.zip") as file1, \
+                zipfile.ZipFile(os.fdopen(os.open("tests/embedding.zip", os.O_RDONLY), "rb"), "r") as file2, \
+                zipfiles.ZIPSource(file1, file1.getinfo("syntax.pyhp"), compiler) as source1, \
+                zipfiles.ZIPSource(file2, file2.getinfo("syntax.pyhp"), compiler) as source2:
+            spec1 = source1.code().spec  # type: ignore
+            spec2 = source2.code().spec  # type: ignore
+        self.assertEqual(spec1.name, "__main__")
+        self.assertEqual(spec1.origin, "tests/embedding.zip/syntax.pyhp")
+        self.assertTrue(spec1.has_location)
+        self.assertEqual(spec2.name, "__main__")
+        self.assertFalse(spec2.has_location)
+
+    def test_source(self) -> None:
+        """test ZIPSource.source"""
+        with open("tests/embedding/syntax.pyhp", "r", newline="\n") as fd, \
+                zipfile.ZipFile("tests/embedding.zip") as file, \
+                zipfiles.ZIPSource(file, file.getinfo("syntax.pyhp"), compiler) as source:
+            self.assertEqual(fd.read(), source.source())
+
+    def test_size(self) -> None:
+        """test ZIPSource.size"""
+        with zipfile.ZipFile("tests/embedding.zip") as file, \
+                zipfiles.ZIPSource(file, file.getinfo("syntax.pyhp"), compiler) as source:
+            self.assertEqual(source.size(), len(source.source()))
+
+    def test_introspection(self) -> None:
+        """test code introspection"""
+        with zipfile.ZipFile("tests/embedding.zip") as file, \
+                zipfiles.ZIPSource(file, file.getinfo("syntax.pyhp"), compiler) as source:
+            spec = source.code().spec   # type: ignore
+            self.assertEqual(
+                "\n".join(source.source().splitlines()) + "\n",
+                inspect.getsource(importlib.util.module_from_spec(spec))
+            )
+        with self.assertRaises(ImportError):
+            spec.loader.get_code("test")
+        with self.assertRaises(ImportError):
+            spec.loader.get_source("test")
+
+    def test_timestamps(self) -> None:
+        """test ZIPSource timestamps"""
+        # allow parallel execution of file reading tests
+        with zipfile.ZipFile("tests/embedding.zip") as file, \
+                zipfiles.ZIPSource(file, file.getinfo("syntax.pyhp"), compiler) as source:
+            mtime_ns = source.mtime()
+            self.assertEqual(mtime_ns, 1613673628000000000)
+            self.assertEqual(source.info(), (mtime_ns, 0, 0))
+
+
+class TestZIPFileContainer(unittest.TestCase):
+    """test ZIPFile"""
+
+    container = zipfiles.ZIPFile(
+        zipfile.ZipFile("tests/embedding.zip", "r"),
+        compiler
+    )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.container.close()
+
+    def test_config(self) -> None:
+        """test ZIPFile.from_config"""
+        self.assertEqual(
+            self.container,
+            zipfiles.ZIPFile.from_config(
+                {
+                    "path": "tests/embedding.zip"
+                },
+                self.container.compiler
+            )
+        )
+        with self.assertRaises(KeyError):
+            zipfiles.ZIPFile.from_config(
+                {},
+                compiler
+            )
+        with self.assertRaises(ValueError):
+            zipfiles.ZIPFile.from_config(
+                {
+                    "path": "tests/embedding.zip",
+                    "mode": 9
+                },
+                compiler
+            )
+        with self.assertRaises(ValueError):
+            zipfiles.ZIPFile.from_config(
+                {
+                    "path": 8,
+                },
+                compiler
+            )
+        with self.assertRaises(ValueError):
+            zipfiles.ZIPFile.from_config(
+                {
+                    "path": "tests/embedding.zip",
+                    "pwd": 7
+                },
+                compiler
+            )
+        with self.assertRaises(ValueError):
+            zipfiles.ZIPFile.from_config(
+                {
+                    "path": "tests/embedding.zip"
+                },
+                self.container
+            )
+
+    def test_access(self) -> None:
+        """test ZIPFile code retrieval"""
+        for name, source in self.container.items():
+            with source:
+                path = os.path.join("tests/embedding", name)
+                with open(path, "r", newline="") as fd:
+                    self.assertEqual(
+                        source.code(),
+                        compiler.compile_raw(fd.read(), source.spec)
+                    )
+
+    def test_iter(self) -> None:
+        """test iter(ZIPFile)"""
+        files = {   # set -> no order
+            "syntax.pyhp",
+            "syntax.output",
+            "indentation.pyhp",
+            "indentation.output",
+            "shebang.pyhp",
+            "shebang.output"
+        }
+        self.assertEqual(
+            files,
+            self.container.keys()
+        )
+
+    def test_eq(self) -> None:
+        """test ZIPFile.__eq__"""
+        with tempfile.NamedTemporaryFile("wb") as file:
+            containers = [
+                self.container,
+                zipfiles.ZIPFile(
+                    self.container.file,
+                    compiler2
+                ),
+                zipfiles.ZIPFile(
+                    zipfile.ZipFile(file, "w"),
+                    self.container.compiler
+                )
+            ]
+            try:
+                self.assertEqual(
+                    [self.container],
+                    [container for container in containers if container == self.container]
+                )
+            finally:
+                containers[-1].close()
